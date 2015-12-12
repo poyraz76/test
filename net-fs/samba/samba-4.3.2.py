a54e77ccc35cd4199594c59dbf6237443a654c10
wdysln <zirkovandersen@gmail.com>
2015-12-12 15:10:15 +0200

    samba

--- net-fs/samba/samba-4.3.2.py
+++ net-fs/samba/samba-4.3.2.py
@@ -9,7 +9,7 @@ arch @ ~x86_64
 depends = """
 runtime @ >=dev-db/sqlite-3.7 net-dns/libidn >=net-libs/gnutls-2.8.3
 build @ dev-util/pkg-config net-dns/openldap dev-libs/libxslt app-arch/libarchive net-libs/gnutls dev-libs/libgpg-error
-        dev-libs/popt sys-apps/dbus sys-libs/talloc sys-libs/pam
+        dev-libs/popt sys-apps/dbus sys-libs/talloc sys-libs/pam app-text/docbook-xsl-stylesheets sys-apps/acl
 """
 MODULES = "idmap_ad,idmap_rid,idmap_adex,idmap_hash,idmap_tdb2,\
 pdb_tdbsam,pdb_ldap,pdb_ads,pdb_smbpasswd,pdb_wbc_sam,pdb_samba4,\
@@ -50,9 +50,29 @@ def configure():
        --with-utmp \
        --with-winbind \
       " % MODULES)
+    
 def build():
     make()
          
 def install():
     raw_install("DESTDIR=%s" % install_dir)
+    insfile("%s/*.service" % filesdir,"/usr/lib/systemd/system/")
+    insfile("%s/*.socket" % filesdir,"/usr/lib/systemd/system/")
+    insfile("%s/samba.conf.d" % filesdir,"/etc/conf.d/samba")
+    insfile("%s/samba.conf" % filesdir,"/usr/lib/tmpfiles.d/samba.conf")
+    
+    insfile("%s/samba.logrotate" % filesdir,"/etc/logrotate.d/samba")
+    insfile("%s/samba.pam" % filesdir,"/etc/pam.d/samba")
 
+    
+    insfile("packaging/LSB/smb.conf", "/etc/samba/smb.conf.default")
+    
+    makedirs("/var/spool/samba")
+    makedirs("/etc/samba/private")
+    setmod("1777", "%s/var/spool/samba" % install_dir)
+    setmod("700", "%s/etc/samba/private" % install_dir)
+    rmdir("%s/var/run" % install_dir)
+     
+def post_install():
+    system("systemd-tmpfiles --create samba.conf")
+    
