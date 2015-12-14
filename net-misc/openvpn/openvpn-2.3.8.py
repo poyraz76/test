11f6808048a0c27329cadef18825e9c982f29e8e
wdysln <zirkovandersen@gmail.com>
2015-12-14 17:52:16 +0200

    firefox

---
+++ net-misc/openvpn/openvpn-2.3.8.py
@@ -0,0 +1,28 @@
+metadata = """
+summary @ An easy-to-use, robust, and highly configurable VPN (Virtual Private Network)
+homepage @ http://openvpn.net/index.php/open-source.html
+license @ custom
+src_url @ http://swupdate.openvpn.net/community/releases/$fullname.tar.gz
+arch @ ~x86_64
+"""
+
+#srcdir = name+"-7.1p1"
+
+depends = """
+runtime @ sys-libs/glibc dev-libs/openssl sys-apps/iproute2 app-arch/lzo sys-apps/systemd
+"""
+
+
+def configure():
+    system('./configure \
+            --prefix=/usr \
+            --enable-password-save \
+            --with-plugindir="\/usr/lib/openvpn/" \
+            --enable-iproute2 \
+            --enable-systemd')
+
+
+def install():
+    raw_install("DESTDIR=%s" % install_dir)
+    
+    insfile("%s/sshdgenkeys.service" % filesdir,"/usr/lib/systemd/system/sshdgenkeys.service")
