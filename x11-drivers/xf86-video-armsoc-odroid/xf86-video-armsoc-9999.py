a461d423b9639f74ab8ba6ea0c2bf790f30fbb8d
wdysln <zirkovandersen@gmail.com>
2015-12-16 20:13:19 +0200

    armsoc

---
+++ x11-drivers/xf86-video-armsoc-odroid/xf86-video-armsoc-9999.py
@@ -0,0 +1,30 @@
+metadata = """
+summary @  ARMSoC Mali FrameBuffer driver for ODROID
+homepage @ https://github.com/mdrjr/xf86-video-armsoc/tree/r4p0
+license @ GPL2
+arch @ ~x86_64
+"""
+
+depends = """
+build @ x11-proto/dri2proto
+"""
+standard_procedure = False
+
+get("python_utils", "git_utils")
+
+def prepare():
+    git_clone("git@github.com:mdrjr/xf86-video-armsoc.git", subdir=name)
+    
+    
+def configure():
+    cd("xf86-video-armsoc")
+    system("./autogen.sh --prefix=/usr --with-drmmode=exynos")
+
+def build():
+    cd("xf86-video-armsoc")
+    make()
+
+def install():
+    cd("xf86-video-armsoc")
+    raw_install("DESTDIR=%s" % install_dir)
+
